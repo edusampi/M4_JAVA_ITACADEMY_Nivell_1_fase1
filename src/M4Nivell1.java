ea518f7705777aa5aafbad49a74c62b90abb9965
edusa <edusampi@gmail.com>
2021-05-05 16:50:24 +0200

    M4_JAVA_ITACADEMY_Nivell_1_fase1

---
+++ src/M4Nivell1.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class M4Nivell1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// FASE 1 ---------------------------
+		// Creació de variables bitllets i arrays de plats i preus
+		
+		int cinc = 5;
+		int deu = 10;
+		int vint = 20;
+		int cinquanta = 50;
+		int cent = 100;
+		int doscents = 200;
+		int cinccents = 500;
+		int preuTotal = 0;
+		
+		String plats []= new String[10];
+		int preus [] = new int[10];			 	
+		
+	}
+
+}
